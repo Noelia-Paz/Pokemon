6662e682e2dd866d58c836a09f55e908f0f42111
noelia-paz <noeliapaz676@gmail.com>
2023-06-10 17:51:09 -0300

    url vercel

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pokemon.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://pokemon-e41u.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
